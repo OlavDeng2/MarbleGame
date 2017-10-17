62933d7274a09186d86cbf4c8e2df7da83d5d99d
OlavDeng2 <olavkruithof@gmail.com>
2017-10-17 14:49:19 +0200

    Attatched camera to the player

--- Assets/MarbleGame/Scripts/PlayerController.cs
+++ Assets/MarbleGame/Scripts/PlayerController.cs
@@ -4,15 +4,38 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
+    //Initialize some variables
+    public int lives = 3;
+    public float speed = 3f;
+
+
 	// Use this for initialization
 	void Start ()
     {
 		
 	}
-	
-	// Update is called once per frame
-	void Update ()
+
+    // Update is called once per frame
+    void Update()
     {
-		
+        if (Input.GetButton("left"))
+        {
+            //Move left
+        }
+
+        if (Input.GetButton("right"))
+        {
+            //move Right
+        }
+
+        if(Input.GetButton("up"))
+        {
+            //Move Up
+        }
+
+        if(Input.GetButton("down"))
+        {
+            //Move Down
+        }
 	}
 }
