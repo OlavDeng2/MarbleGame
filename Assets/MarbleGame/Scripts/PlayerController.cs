8844f2cb74bd6cc48c6990ea8625361a2f115dfc
OlavDeng2 <olavkruithof@gmail.com>
2017-10-24 14:35:54 +0200

    It now reads input, yay

--- Assets/MarbleGame/Scripts/PlayerController.cs
+++ Assets/MarbleGame/Scripts/PlayerController.cs
@@ -10,32 +10,37 @@ public class PlayerController : MonoBehaviour {
 
 
 
-	// Use this for initialization
-	void Start ()
+    // Use this for initialization
+    void Start()
     {
-		
-	}
+
+    }
 
     // Update is called once per frame
     void Update()
     {
+    
         if (Input.GetButton("left"))
         {
+            print("Moving Left");
             //Move left
         }
 
         if (Input.GetButton("right"))
         {
+            print("Moving right");
             //move Right
         }
 
         if(Input.GetButton("up"))
         {
+            print("Moving up");
             //Move Up
         }
 
         if(Input.GetButton("down"))
         {
+            print("Moving down");
             //Move Down
         }
 	}
