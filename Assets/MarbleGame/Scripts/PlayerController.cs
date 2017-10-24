6ec80a9c24e66e54183bceb2e5463b2c63b07edb
OlavDeng2 <olavkruithof@gmail.com>
2017-10-24 15:02:40 +0200

    Moved movement code to own function and checks if it is main menu

--- Assets/MarbleGame/Scripts/PlayerController.cs
+++ Assets/MarbleGame/Scripts/PlayerController.cs
@@ -7,6 +7,8 @@ public class PlayerController : MonoBehaviour {
     //Initialize some variables
     public int lives = 3;
     public float speed = 3f;
+
+    public bool isMainMenu = true;
     
 
 
@@ -19,33 +21,40 @@ public class PlayerController : MonoBehaviour {
     // Update is called once per frame
     void Update()
     {
-    
-        if (Input.GetButton("left"))
-        {
-            print("Moving Left");
-            //Move left
-            gameObject.GetComponent<Rigidbody>().AddForce(Vector3.left * speed);
-        }
-
-        if (Input.GetButton("right"))
-        {
-            print("Moving right");
-            //move Right
-            gameObject.GetComponent<Rigidbody>().AddForce(Vector3.right * speed);
-        }
-
-        if(Input.GetButton("up"))
-        {
-            print("Moving up");
-            //Move Up
-            gameObject.GetComponent<Rigidbody>().AddForce(Vector3.forward * speed);
-        }
-
-        if(Input.GetButton("down"))
-        {
-            print("Moving down");
-            //Move Down
-            gameObject.GetComponent<Rigidbody>().AddForce(Vector3.back * speed);
-        }
+        MarbleInput(isMainMenu);   
 	}
+
+    void MarbleInput(bool isMenu)
+    {
+        if (isMenu)
+        {
+            if (Input.GetButton("left"))
+            {
+                print("Moving Left");
+                //Move left
+                gameObject.GetComponent<Rigidbody>().AddForce(Vector3.left * speed);
+            }
+
+            if (Input.GetButton("right"))
+            {
+                print("Moving right");
+                //move Right
+                gameObject.GetComponent<Rigidbody>().AddForce(Vector3.right * speed);
+            }
+
+            if (Input.GetButton("up"))
+            {
+                print("Moving up");
+                //Move Up
+                gameObject.GetComponent<Rigidbody>().AddForce(Vector3.forward * speed);
+            }
+
+            if (Input.GetButton("down"))
+            {
+                print("Moving down");
+                //Move Down
+                gameObject.GetComponent<Rigidbody>().AddForce(Vector3.back * speed);
+            }
+        }
+    }
 }
