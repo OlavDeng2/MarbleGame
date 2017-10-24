db178e8ed14ae4f5750827e3c4d8f41791763ec5
OlavDeng2 <olavkruithof@gmail.com>
2017-10-24 14:43:26 +0200

    Movement now works

--- Assets/MarbleGame/Scripts/PlayerController.cs
+++ Assets/MarbleGame/Scripts/PlayerController.cs
@@ -7,7 +7,7 @@ public class PlayerController : MonoBehaviour {
     //Initialize some variables
     public int lives = 3;
     public float speed = 3f;
-
+    
 
 
     // Use this for initialization
@@ -24,24 +24,28 @@ public class PlayerController : MonoBehaviour {
         {
             print("Moving Left");
             //Move left
+            gameObject.GetComponent<Rigidbody>().AddForce(Vector3.left * speed);
         }
 
         if (Input.GetButton("right"))
         {
             print("Moving right");
             //move Right
+            gameObject.GetComponent<Rigidbody>().AddForce(Vector3.right * speed);
         }
 
         if(Input.GetButton("up"))
         {
             print("Moving up");
             //Move Up
+            gameObject.GetComponent<Rigidbody>().AddForce(Vector3.forward * speed);
         }
 
         if(Input.GetButton("down"))
         {
             print("Moving down");
             //Move Down
+            gameObject.GetComponent<Rigidbody>().AddForce(Vector3.back * speed);
         }
 	}
 }
