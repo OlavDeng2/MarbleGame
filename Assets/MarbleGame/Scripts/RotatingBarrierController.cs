485595ba9b5ade20dbb5c3dfc58eba7046a3b1d7
OlavDeng2 <olavkruithof@gmail.com>
2017-10-31 20:10:27 +0100

    made the rotating barrier move and also stop on click

--- Assets/MarbleGame/Scripts/RotatingBarrierController.cs
+++ Assets/MarbleGame/Scripts/RotatingBarrierController.cs
@@ -4,17 +4,26 @@ using UnityEngine;
 
 public class RotatingBarrierController : MonoBehaviour {
 
-    public float rotationSpeed = 0f;
+    //rotation for rotating the object
+    public Vector3 rotation = new Vector3(0,0,0);
+
+    //Bool for if the rotating barrier can rotate
+    bool canRotate = false;
 
     private void FixedUpdate()
     {
-
+        if(canRotate)
+        {
+            gameObject.GetComponent<Transform>().Rotate(rotation);
+        }
     }
 
     private void OnMouseDown()
     {
-        //Do stuff
-        gameObject.GetComponent<Transform>().rotate;
+        //Start rotating if it isnt allready rotating, if it is rotating, stop rotating
+        if(!canRotate){canRotate = true;}
+        else if(canRotate) { canRotate = false; }
+
 
         print("Game Object clicked");
     }
