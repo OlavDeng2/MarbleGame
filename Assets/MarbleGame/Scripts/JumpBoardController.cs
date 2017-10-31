5fab8b87aff89b7de13e4ad49cb8a9c41cea60bd
OlavDeng2 <olavkruithof@gmail.com>
2017-10-31 11:08:36 +0100

    Boop

--- Assets/MarbleGame/Scripts/JumpBoardController.cs
+++ Assets/MarbleGame/Scripts/JumpBoardController.cs
@@ -7,5 +7,7 @@ public class JumpBoardController : MonoBehaviour
     private void OnMouseDown()
     {
         //Do stuff
+        gameObject.GetComponent<Rigidbody>().AddForce(new Vector3(0, 0, 5));
+        print("Game Object clicked");
     }
 }
