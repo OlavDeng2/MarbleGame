57a30e318c061f5a6f05d78d8960e8b17953d165
OlavDeng2 <olavkruithof@gmail.com>
2017-10-31 19:37:35 +0100

    Created basic Jumpboard(that is also functional)

--- Assets/MarbleGame/Scripts/JumpBoardController.cs
+++ Assets/MarbleGame/Scripts/JumpBoardController.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class JumpBoardController : MonoBehaviour
 {
+    public Vector3 force = new Vector3(0,0,0);
+
     private void OnMouseDown()
     {
         //Do stuff
-        gameObject.GetComponent<Rigidbody>().AddForce(new Vector3(0, 0, 5));
+        gameObject.GetComponent<Rigidbody>().AddForce(new Vector3(0, 5000, 0));
         print("Game Object clicked");
     }
 }
