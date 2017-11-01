135ee3a6404c91e93ff6154f717af45351fb8dd0
OlavDeng2 <olavkruithof@gmail.com>
2017-11-01 10:11:55 +0100

    created trapdoor and trapdoor controller

---
+++ Assets/MarbleGame/Scripts/TrapdoorController.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrapdoorController : MonoBehaviour {
+
+    private void OnMouseDown()
+    {
+        print("Game Object clicked");
+    }
+}
